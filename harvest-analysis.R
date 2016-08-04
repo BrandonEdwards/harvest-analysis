17bf220ca6a0173cf860919890e7288beef9f19e
Brandon Edwards <brandon.edwards144@gmail.com>
2016-08-04 10:05:04 -0400

    Lake analysis

--- harvest-analysis.R
+++ harvest-analysis.R
@@ -18,3 +18,30 @@ remove(list=ls())
 
 #Should be currentYear - 1
 AnalysisYear <- 2015
+
+####################################
+# File IO
+####################################
+
+mainDir <- paste(getwd(), "/", sep = "")
+subDir <- paste("Plots/", sep = "")
+dir.create(file.path(mainDir, subDir))
+
+####################################
+#	Load Data
+####################################	
+
+data <- read.csv(paste(mainDir, AnalysisYear, "_Harvest_Data_All.csv", sep=""))
+
+####################################
+#	Analyze Entire Lake
+####################################
+
+lake.results <- NULL
+
+for (month in 1:12)
+{
+  data.temp <- data[data$MONTH %in% c(month), ]
+  total.weight <- sum(data.temp$HVSWT_KG)
+  lake.results <- rbind(lake.results, (month, total.weight))
+}
