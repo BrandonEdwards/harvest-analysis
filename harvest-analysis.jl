541de5543237ccf7955a1cd3eb0694a08625416a
Brandon Edwards <brandon.edwards144@gmail.com>
2016-08-04 10:04:53 -0400

    Analysis in Julia

---
+++ harvest-analysis.jl
@@ -0,0 +1,48 @@
+#############################
+#harvest-analysis.jl
+#Brandon Edwards
+#Last Updated: August 2016
+#############################
+
+#############################
+# Constants
+#############################
+
+#Should be current year - 1
+analysisYear = 2015
+
+#############################
+# Packages needed
+#############################
+
+using DataFrames, Gadfly
+
+#############################
+# File IO
+#############################
+projPath = Base.source_path()
+separateDirChar = "\\"
+pathEndSearch = rsearch(projPath, separateDirChar)[1]
+endOfDirPath = projPath[pathEndSearch:length(projPath)]
+projPath = split(projPath, endOfDirPath)[1]
+file = string(projPath, "\\", analysisYear, "_Harvest_Data_All.csv")
+
+#############################
+# Import Data
+#############################
+
+data = readtable(file)
+#Drop data with NA for month
+data = data[!isna(data[:MONTH]),:]
+
+#############################
+# Entire Lake Analysis
+#############################
+lakeResults = DataFrame(Month = Int[], HarvestWeight = Float64[], Events = Int64[])
+size(data)[1]
+for month = 1:12
+  tempData = data[data[:MONTH] .== month, :]
+  push!(lakeResults, (month, sum(tempData[:HVSWT_KG]), size(tempData)[1]))
+end
+
+lakeResults
